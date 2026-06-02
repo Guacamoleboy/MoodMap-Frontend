b6d8db353ad8d74874aba8034462b9bc3d08f33a
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-06-02 13:25:19 +0200

    WontAdd routing fix.

--- frontend/src/components/shared/wont-add/WontAddActions.jsx
+++ frontend/src/components/shared/wont-add/WontAddActions.jsx
@@ -2,15 +2,18 @@
 // _______
 // src/components/shared/wont-add/WontAddActions.jsx
 
+import { Link } from 'react-router-dom'
 import BackOrLogin from '@components/shared/BackOrLogin'
 import styles from './WontAdd.module.css'
 
 const WontAddActions = () => (
     <div className={styles.wrapper}>
         <BackOrLogin size="l" />
-        <button className="moodmap-button l secondary">
-            Gå til Devlog
-        </button>
+        <Link to="/devlog">
+            <button className="moodmap-button l secondary">
+                Gå til Devlog
+            </button>
+        </Link>
     </div>
 )
 
